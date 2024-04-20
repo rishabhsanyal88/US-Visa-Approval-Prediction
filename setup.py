8bf0fe709a01a5c490dbbb21e9cbc475b3fe17b4
Rishabh Sanyal <datasciencerishi@outlook.com>
2024-04-20 18:44:22 +0530

    Setup project package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="rishabh",
+    author_email="datasciencerishi@outlook.com",
+    packages=find_packages()
+)
